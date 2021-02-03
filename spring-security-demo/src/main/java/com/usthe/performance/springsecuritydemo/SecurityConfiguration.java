911cc1b178d2bbe14229496ba2a4aa04658253b4
tomsun28 <tomsun28@outlook.com>
2021-02-03 21:41:14 +0800

    update api ant chain

--- spring-security-demo/src/main/java/com/usthe/performance/springsecuritydemo/SecurityConfiguration.java
+++ spring-security-demo/src/main/java/com/usthe/performance/springsecuritydemo/SecurityConfiguration.java
@@ -18,11 +18,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(username -> {
-            return User.withUsername("root")
-                    // 密码使用BCryptPasswordEncoder()方法验证，因此存储的密码也需要BCryptPasswordEncoder().encode(明文密码)方法加密密码
-                    .password(new BCryptPasswordEncoder().encode("23456")).roles("role2").build();
-        }).passwordEncoder(new BCryptPasswordEncoder());
+        // 密码使用BCryptPasswordEncoder()方法验证，
+        // 因此存储的密码也需要BCryptPasswordEncoder().encode(明文密码)方法加密密码
+        auth.userDetailsService(username -> User.withUsername("root")
+                .password(new BCryptPasswordEncoder().encode("23456"))
+                .roles("role2").build()).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
@@ -33,32 +33,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/**/*.js").anonymous()
                 .antMatchers(HttpMethod.GET, "/**/*.css").anonymous()
                 .antMatchers(HttpMethod.GET, "/**/*.icon").anonymous()
-                .antMatchers(HttpMethod.GET, "/api/v1/source3").anonymous()
-                .antMatchers(HttpMethod.POST, "/api/v1/account/auth").anonymous()
-
-                .antMatchers(HttpMethod.GET, "/api/v1/source1").hasRole("role2")
-                .antMatchers(HttpMethod.DELETE, "/api/v1/source1").hasRole("role3")
-                .antMatchers(HttpMethod.PUT, "/api/v1/source1").hasAnyRole("role1","role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source2").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/v1/source2/*/*").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source3").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source3/*").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source4").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source5").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source6").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source7").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source8").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source9").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source10").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source11").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source12").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source13").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source14").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source15").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source16").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source17").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source18").hasRole("role2")
-                .antMatchers(HttpMethod.GET, "/api/v1/source19").hasRole("role2")
                 .antMatchers(HttpMethod.GET, "/api/v9/source1").hasRole("role2")
                 .antMatchers(HttpMethod.GET, "/api/v9/source2").hasRole("role2")
                 .antMatchers(HttpMethod.GET, "/api/v9/source3").hasRole("role2")
@@ -211,6 +185,31 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/api/v2/source17").hasRole("role2")
                 .antMatchers(HttpMethod.GET, "/api/v2/source18").hasRole("role2")
                 .antMatchers(HttpMethod.GET, "/api/v2/source19").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source3").anonymous()
+                .antMatchers(HttpMethod.POST, "/api/v1/account/auth").anonymous()
+                .antMatchers(HttpMethod.GET, "/api/v1/source1").hasRole("role2")
+                .antMatchers(HttpMethod.DELETE, "/api/v1/source1").hasRole("role3")
+                .antMatchers(HttpMethod.PUT, "/api/v1/source1").hasAnyRole("role1","role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source2").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/source2/*/*").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source3").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source3/*").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source4").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source5").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source6").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source7").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source8").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source9").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source10").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source11").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source12").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source13").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source14").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source15").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source16").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source17").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source18").hasRole("role2")
+                .antMatchers(HttpMethod.GET, "/api/v1/source19").hasRole("role2")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
